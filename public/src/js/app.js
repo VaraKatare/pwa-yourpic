bd2a5de7b1a18d6c40a080fd76c158edbd76dcb3
VaraKatare <fathur.fathur7@gmail.com>
2018-11-30 22:17:12 +0700

    make app offline mode

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,11 +1,18 @@
 
 var deferredPrompt;
 
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/sw.js')
     .then(function() {
       console.log('Service Worker has been registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
     });
 }
 
